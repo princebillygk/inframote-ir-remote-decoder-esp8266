3358b76ce26c3d4a82de9a5bf1124e781b521e06
princebillygk <princebillygk@gmail.com>
2022-05-01 15:47:00 +0600

    Modularized and documented

---
+++ src/splash.h
@@ -0,0 +1,20 @@
+#include<Adafruit_SSD1306.h>
+
+#ifndef SPLASH_H
+#define SPLASH_H
+
+/*!
+    @brief  Loads Inframote Splash Screen on a SSD1306 display
+    @param  display
+            An Adafruit_SSD1306 Instance
+    @param  color
+            Indicates the color of the splash screen text and objects
+    @param  bgColor
+            Indicates the background color of splash screen
+    @note   This function will work with any kind of display using
+            Adafruit_GFX library with some modification
+    
+*/
+void loadSplashScreen(Adafruit_SSD1306* display, uint16_t color, uint16_t bgColor);
+
+#endif
