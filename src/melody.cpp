e59ef8b3c7563485850eae8a93f0672df9e8753f
princebillygk <princebillygk@gmail.com>
2022-05-12 01:07:16 +0600

    improved intro music

--- src/melody.cpp
+++ src/melody.cpp
@@ -1,36 +1,31 @@
 #include <Arduino.h>
 
 #include "melody.h"
+#include "pitch.h"
 
 using namespace melody;
 
 void melody::intro(uint8_t pin) {
-  tone(pin, 2049, 201.6125);
-  delay(201.6125);
-  tone(pin, 2029, 201.6125);
-  delay(201.6125);
-  tone(pin, 1961, 201.6125);
-  delay(201.6125);
-  tone(pin, 1864, 201.6125);
-  delay(201.6125);
-  tone(pin, 2140, 201.6125);
-  delay(201.6125);
-  tone(pin, 1946, 201.6125);
-  delay(201.6125);
-  tone(pin, 1895, 201.6125);
-  delay(201.6125);
-  tone(pin, 2091, 201.6125);
-  delay(201.6125);
-  tone(pin, 1946, 201.6125);
-  delay(201.6125);
-  tone(pin, 1961, 201.6125);
-  delay(201.6125);
-  tone(pin, 1946, 201.6125);
-  delay(201.6125);
-  tone(pin, 1961, 201.6125);
-  delay(201.6125);
-  tone(pin, 1946, 201.6125);
-  delay(201.6125);
-  tone(pin, 1961, 201.6125);
-  delay(201.6125);
+  unsigned long d = 250;
+  tone(pin, NOTE_F5);
+  delay(d);
+  tone(pin, NOTE_E5);
+  delay(d);
+  tone(pin, NOTE_C5);
+  delay(d);
+  tone(pin, NOTE_E4);
+  delay(d);
+  tone(pin, NOTE_A5);
+  delay(d);
+  tone(pin, NOTE_B4);
+  delay(d);
+  tone(pin, NOTE_G4);
+  delay(d);
+  tone(pin, NOTE_G5);
+  delay(d);
+  tone(pin, NOTE_B4);
+  delay(d);
+  tone(pin, NOTE_C5);
+  delay(d);
+  noTone(pin);
 }
