2650c3b99b5b624e803155e5e79c2fea9abae7f8
princebillygk <princebillygk@gmail.com>
2022-07-14 21:32:28 +0600

    successfully loaded the splashscreen

--- lib/Check/check.h
+++
@@ -1,7 +0,0 @@
-#ifndef CHECK_H
-#define CHECK_H
-/*!
-    @brief Simple setup functions to check if the programmer is working
-*/
-void checkProgrammer();
-#endif
--- src/main.cpp
+++ src/main.cpp
@@ -9,16 +9,15 @@
 #include <Wire.h>
 #include <Adafruit_SSD1306.h>
 
-#include <IRremote.hpp>
+// #include <IRremoteESP8266.h>
 
-#include "check.h"
 #include "splash.h"
-#include "screens.h"
-#include "melody.h"
+// #include "screens.h"
+// #include "melody.h"
 
 // OLED Specifications ---
 #define OLED_ADDR 0x3C
-#define OLED_RST 4
+#define OLED_RST -1
 #define OLED_W 128
 #define OLED_H 64
 
@@ -37,47 +36,22 @@
 Adafruit_SSD1306 display(OLED_W, OLED_H, &Wire, OLED_RST);
 
 void setup() {
-/**
-checkProgrammer();
-/*/
-  Serial.begin(9600);
-  if (!display.begin(SSD1306_SWITCHCAPVCC, OLED_ADDR, OLED_RST)) {
+  Serial.begin(115200);
+  if(!display.begin(SSD1306_SWITCHCAPVCC, 0x3C)) { 
     Serial.println("Failed to communicate with SSD1306 Display");
-    while (1);
+    while (true);
   }
 
   loadAnimatedSplashScreen(&display, CONTENT_COLOR, BG_COLOR);
-  loadWelcomeText(&display, 1, 0);
-  melody::intro(PIEZO_PIN);
+  // loadWelcomeText(&display, 1, 0);
+  // melody::intro(PIEZO_PIN);
 
-  // Turn off screen
-  display.clearDisplay();
-  display.display();
-  //*/
-
-  IrReceiver.begin(IR_RECEIVE_PIN, ENABLE_LED_FEEDBACK);
-  IrSender.begin(IR_SEND_PIN, ENABLE_LED_FEEDBACK);
+  // display.clearDisplay();
+  // display.display();
+  while(true);
 }
 
 void loop() {
-  loadWaitingScreen(&display, CONTENT_COLOR, BG_COLOR, 1);
-  while (true) {
-        if (IrReceiver.decode()) {
-        Serial.println(IrReceiver.decodedIRData.decodedRawData, HEX);
-        IrReceiver.printIRResultShort(&Serial); // optional use new print version
-        IrReceiver.resume(); // Enable receiving of the next value
-        break;
-    }
-  }
-  delay(5000);
-  loadTestScreen(
-    &display,
-    CONTENT_COLOR,
-    BG_COLOR,
-    1,
-    IrReceiver.decodedIRData.protocol,
-    IrReceiver.decodedIRData.address,
-    IrReceiver.decodedIRData.command
-  );
-  while (true);
+  // loadWaitingScreen(&display, CONTENT_COLOR, BG_COLOR, 1);
+  // while(true);
 }
